6ae1df70793661805497ce0cd1592d912ceaea45
cristian <cristian.ortiz@uda.cl>
2021-04-06 22:34:24 -0400

    first commit create appm basic config and first components layout

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Header = () => {
+  return (
+    <header className="bg-alert">
+      <h1>Find Cocktails Recipes</h1>
+    </header>
+  );
+};
+
+export default Header;
