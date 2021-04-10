e3a6d40da6ef0797817271036a42373c9288be73
cristian <cristian.ortiz@uda.cl>
2021-04-10 15:11:24 -0400

    finish the modal context, adding material UI to show a modal window whit more info about a recipe when the user make click on it

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,9 +1,63 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { ModalContext } from "../context/ModalContext";
 
+import Modal from "@material-ui/core/modal";
+import { makeStyles } from "@material-ui/core/styles";
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 450,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 const Recipe = ({ recipe }) => {
+  //modal config from materialUI
+  const [modalStyle] = useState(getModalStyle);
+  const [open, setOpen] = useState(false);
+
+  const classes = useStyles();
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  }; //---End config materialUI modal
+
   //destructuring and using state handle function from ModalContext
-  const { handleRecipeId } = useContext(ModalContext);
+  const { full_recipe, handleRecipeId, handleFullRecipe } = useContext(
+    ModalContext
+  );
+  //bad API structure we need to loop and show the ingredients of a recipe
+  const showIngredients = (full_recipe) => {
+    let ingredients = [];
+    //api retrieve 15 fix ingredients and some are null
+    for (let i = 1; i < 16; i++) {
+      if (full_recipe[`strIngredient${i}`]) {
+        ingredients.push(
+          <li>
+            {full_recipe[`strMeasure${i}`]} of{" "}
+            {full_recipe[`strIngredient${i}`]}
+          </li>
+        );
+      }
+    }
+    return ingredients;
+  };
 
   return (
     <div className="col-md-4 bm-3">
@@ -17,14 +71,34 @@ const Recipe = ({ recipe }) => {
         <div className="card-body">
           <button
             onClick={() => {
-              //store the id of a recipe in ModalContext state
-              handleRecipeId(recipe.idDrink);
+              handleRecipeId(recipe.idDrink); //store the id of a recipe in ModalContext state
+              handleOpen(); //set open state to true to show modal window
             }}
             type="button"
             className="btn btn-block btn-primary"
           >
             See Recipe
           </button>
+          <Modal
+            open={open}
+            onClose={() => {
+              handleRecipeId(null); //reset recipe_id state to null
+              handleFullRecipe({}); //reset full_recipe state to empty object
+              handleClose(); //reset open state to false to close modal window
+            }}
+          >
+            <div style={modalStyle} className={classes.paper}>
+              <h2>{full_recipe.strDrink} </h2>
+              <h3 className="mt-4">How to make it</h3>
+              <p>{full_recipe.strInstructions}</p>
+              <img
+                className="img-fluid my-4"
+                src={full_recipe.strDrinkThumb}
+              ></img>
+              <h3> Ingredients</h3>
+              <ul>{showIngredients(full_recipe)}</ul>
+            </div>
+          </Modal>
         </div>
       </div>
     </div>
--- src/context/ModalContext.js
+++ src/context/ModalContext.js
@@ -1,4 +1,5 @@
-import React, { createContext, useState } from "react";
+import axios from "axios";
+import React, { createContext, useEffect, useState } from "react";
 
 //context reference
 export const ModalContext = createContext();
@@ -6,9 +7,24 @@ export const ModalContext = createContext();
 const ModalProvider = (props) => {
   //state for recipesId when the user click on see recipe button
   const [recipe_id, handleRecipeId] = useState(null);
+  //state for the full recipe info retrieved from the API
+  const [full_recipe, handleFullRecipe] = useState({});
+
+  //once there is  a recipe id in the recipe_id state request the API to get full recipe info
+  useEffect(() => {
+    const getFullRecipe = async () => {
+      if (!recipe_id) return;
+      const url = `https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${recipe_id}`;
+      const response = await axios.get(url);
+      handleFullRecipe(response.data.drinks[0]); //state the api response in full_recipe
+    };
+    getFullRecipe();
+  }, [recipe_id]);
 
   return (
-    <ModalContext.Provider value={{ handleRecipeId }}>
+    <ModalContext.Provider
+      value={{ full_recipe, handleRecipeId, handleFullRecipe }}
+    >
       {props.children}
     </ModalContext.Provider>
   );
